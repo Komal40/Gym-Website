05f93a2bab54d22edf90b0ed95f5e6982a0bf1ab
unknown <singh777komal@gmail.com>
2023-07-02 12:50:25 +0530

    changes

--- src/pages/Register/Register.jsx
+++ src/pages/Register/Register.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
-import { Route, Routes } from 'react-router-dom'
 import Login from '../Login/Login'
-import Registration from './Registration'
+
 
 export default function Register() {
   return (
